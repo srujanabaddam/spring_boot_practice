5081778a8b73ba0797a63bdbe39f0762f5a5a14f
srujana <mailme2srujana@gmail.com>
2021-05-28 18:39:13 -0400

    Spring boot profile practice 3

---
+++ practise-3/src/main/java/com/cah/spring/web/practise3/ProfilesApplication.java
@@ -0,0 +1,12 @@
+package com.cah.spring.web.practise3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilesApplication.class, args);
+	}
+}
